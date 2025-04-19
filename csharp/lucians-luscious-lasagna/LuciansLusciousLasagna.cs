5af486478001a2fbedcbd39cd11655eb9e94bb99
Didier Martínez <didier.martinez@sinco.com.co>
2025-04-19 16:33:03 -0500

    Lasagna kata

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,10 +1,10 @@
 class Lasagna
 {
-    // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven() => 40;
 
-    // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int currentMinutes) => ExpectedMinutesInOven() - currentMinutes;
 
-    // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int layers) => layers * 2;
 
-    // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int layers, int minutesOven) => PreparationTimeInMinutes(layers) + minutesOven;
 }
